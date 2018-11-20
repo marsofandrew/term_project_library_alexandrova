36404ad64b4cd3ac0ced27b4d493ab7d6fc89aa9
Andrew Petrov (marsofandrew) <marsofandrew@gmail.com>
2018-11-20 21:31:02 +0300

    add new function to WorkCondition
    now we can ask: should we generate new orders or no?

--- library/include/interfaces/WorkCondition.hpp
+++ library/include/interfaces/WorkCondition.hpp
@@ -19,6 +19,11 @@ public:
                               const std::shared_ptr<GeneratorPool> &generatorPool,
                               const std::shared_ptr<ProcessorPool> &processorPool,
                               const std::shared_ptr<Buffer> &buffer) const = 0;
+
+  virtual bool shouldGenerateNewOrders(const std::shared_ptr<Timer> &timer,
+                                       const std::shared_ptr<GeneratorPool> &generatorPool,
+                                       const std::shared_ptr<ProcessorPool> &processorPool,
+                                       const std::shared_ptr<Buffer> &buffer) const = 0;
 };
 
 #endif //TERM_PROJECT_LIBRARY_ALEXANDROVA_WORKCONDITION_HPP
